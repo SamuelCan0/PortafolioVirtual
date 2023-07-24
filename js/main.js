6082f1c3450bb8c728f4c4b230f267106d73bd7e
SamuelCan0 <101413221+SamuelCan0@users.noreply.github.com>
2023-07-24 17:03:55 -0600

    displat y scroll al contenido

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ const btnToggle=document.querySelector('.toggle-btn');
 btnToggle.addEventListener('click',function () {
     document.getElementById('sidebar').classList.toggle('active')
     document.getElementById('contenido').classList.toggle('active')
+    document.getElementById('acercaDe').classList.toggle('active')
+    document.getElementById('experiencia').classList.toggle('active')
 })
